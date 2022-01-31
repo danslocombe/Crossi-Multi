6c5939a73b179cd6610d2f4fcb741ed2314478c6
Dan Slocombe <dan.slocombe@hotmail.com>
2022-01-31 20:03:48 +0000

    Fix dust depth

--- web-client/js/components/visual_effects.js
+++ web-client/js/components/visual_effects.js
@@ -37,7 +37,7 @@ export function create_dust(x, y) {
     return {
         frame_id : Math.floor(Math.random() * spr_smoke_count),
         scale : 0.5 + Math.random() * 0.6,
-        dynamic_depth : y-32,
+        dynamic_depth : SCALE*y-32,
         x : x,
         y : y,
         tick : function() {
