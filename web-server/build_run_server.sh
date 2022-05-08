f045251911047d9514cc8c9b0f1832df4abce700
Ubuntu <soweliadmin@Soweli.cnjabll2mx2ezgfyymflbqwaag.zx.internal.cloudapp.net>
2022-05-08 22:29:58 +0000

    Add build scripts

---
+++ web-server/build_run_server.sh
@@ -0,0 +1,7 @@
+cd ../web-client
+./publish.sh
+cd ../web-server
+cargo_exe=/home/soweliadmin/.cargo/bin/cargo
+key_path=/etc/letsencrypt/live/roadtoads.io/privkey.pem
+cert_path=/etc/letsencrypt/live/roadtoads.io/fullchain.pem
+sudo $cargo_exe +stable run --release -- ~/crossy_multi/serve $key_path $cert_path
---
+++ web-server/run_server.sh
@@ -0,0 +1,6 @@
+cargo_exe=/home/soweliadmin/.cargo/bin/cargo
+key_path=/etc/letsencrypt/live/roadtoads.io/privkey.pem
+cert_path=/etc/letsencrypt/live/roadtoads.io/fullchain.pem
+RUSTFLAGS="--cfg tokio_unstable"
+echo $RUSTFLAGS
+sudo $cargo_exe +stable run --release -- ~/crossy_multi/serve $key_path $cert_path
