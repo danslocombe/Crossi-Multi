90b6fdd9ee22c79de1f11ff289414eafe0d8e967
Dan Slocombe <dan.slocombe@hotmail.com>
2022-10-30 15:06:23 +0000

    Extra logging

--- web-server/src/crossy_server.rs
+++ web-server/src/crossy_server.rs
@@ -1,13 +1,12 @@
-use std::time::{Duration, Instant};
-use serde::{Serialize, Deserialize};
-use tokio::sync::Mutex;
 use chrono::prelude::*;
+use serde::{Deserialize, Serialize};
+use std::time::{Duration, Instant};
+use tokio::sync::Mutex;
 
 use crossy_multi_core::game;
-use crossy_multi_core::timeline::{Timeline, RemoteInput, RemoteTickState};
 use crossy_multi_core::interop::*;
 use crossy_multi_core::player_id_map::PlayerIdMap;
-
+use crossy_multi_core::timeline::{RemoteInput, RemoteTickState, Timeline};
 
 const SERVER_VERSION: u8 = 1;
 const DESIRED_TICK_TIME: Duration = Duration::from_millis(14);
@@ -17,91 +16,92 @@ pub struct SocketId(pub u32);
 
 struct PlayerClient {
     id: game::PlayerId,
-    last_tick_us : u32,
+    last_tick_us: u32,
 }
 
 struct Client {
-    player_client : Option<PlayerClient>,
-    socket_id : SocketId,
+    player_client: Option<PlayerClient>,
+    socket_id: SocketId,
 }
 
 pub struct Server {
-    queued_messages : Mutex<Vec<(CrossyMessage, SocketId, Instant)>>,
-    pub inner : Mutex<ServerInner>,
+    queued_messages: Mutex<Vec<(CrossyMessage, SocketId, Instant)>>,
+    pub inner: Mutex<ServerInner>,
 
-    outbound_tx : tokio::sync::broadcast::Sender<CrossyMessage>,
-    outbound_rx : tokio::sync::broadcast::Receiver<CrossyMessage>,
+    outbound_tx: tokio::sync::broadcast::Sender<CrossyMessage>,
+    outbound_rx: tokio::sync::broadcast::Receiver<CrossyMessage>,
 }
 
 pub struct ServerInner {
-    game_id : crate::GameId,
-    empty_ticks : u32,
-    new_players : Vec<game::PlayerId>,
+    game_id: crate::GameId,
+    empty_ticks: u32,
+    new_players: Vec<game::PlayerId>,
     start: Instant,
-    start_utc : DateTime<Utc>,
+    start_utc: DateTime<Utc>,
     prev_tick: Instant,
     clients: Vec<Client>,
     timeline: Timeline,
-    next_socket_id : SocketId,
-    pub ended : bool,
+    next_socket_id: SocketId,
+    pub ended: bool,
 }
 
 impl Server {
-    pub fn new(id : &crate::GameId) -> Self {
+    pub fn new(id: &crate::GameId) -> Self {
         let start = Instant::now();
-        let start_utc = Utc::now(); 
+        let start_utc = Utc::now();
         let (outbound_tx, outbound_rx) = tokio::sync::broadcast::channel(16);
 
         Server {
-            queued_messages : Mutex::new(Vec::new()),
+            queued_messages: Mutex::new(Vec::new()),
             outbound_tx,
             outbound_rx,
-            inner : Mutex::new(ServerInner {
-                game_id : id.clone(),
-                empty_ticks : 0,
+            inner: Mutex::new(ServerInner {
+                game_id: id.clone(),
+                empty_ticks: 0,
                 clients: Vec::new(),
                 new_players: Vec::new(),
                 timeline: Timeline::from_seed(&id.0),
                 prev_tick: start,
                 start,
                 start_utc,
-                next_socket_id : SocketId(0),
-                ended : false,
+                next_socket_id: SocketId(0),
+                ended: false,
             }),
         }
     }
 
-    pub async fn queue_message(&self, message : CrossyMessage, player : SocketId) {
+    pub async fn queue_message(&self, message: CrossyMessage, player: SocketId) {
         let now = Instant::now();
         match message {
             CrossyMessage::TimeRequestPacket(time_request) => {
                 // Special case hanling for time requests to track the exact time we received the message.
 
                 let inner_guard = self.inner.lock().await;
-                let server_receive_time_us = now.saturating_duration_since(inner_guard.start).as_micros() as u32;
+                let server_receive_time_us =
+                    now.saturating_duration_since(inner_guard.start).as_micros() as u32;
                 drop(inner_guard);
 
                 let new_message = CrossyMessage::TimeRequestIntermediate(TimeRequestIntermediate {
                     server_receive_time_us,
                     client_send_time_us: time_request.client_send_time_us,
-                    socket_id : player.0,
+                    socket_id: player.0,
                 });
 
                 let mut queue_guard = self.queued_messages.lock().await;
                 queue_guard.push((new_message, player, now));
-            },
+            }
             _ => {
                 let mut guard = self.queued_messages.lock().await;
                 guard.push((message, player, now));
-            },
+            }
         }
     }
 
     pub async fn get_server_description(&self) -> ServerDescription {
         let inner = self.inner.lock().await;
         ServerDescription {
-            server_version : SERVER_VERSION,
-            seed : inner.timeline.map.get_seed(),
+            server_version: SERVER_VERSION,
+            seed: inner.timeline.map.get_seed(),
         }
     }
 
@@ -123,8 +123,11 @@ impl Server {
         inner.start_utc.to_string()
     }
 
-    pub async fn play(&self, hello : &ClientHello, socket_id: SocketId) -> Option<InitServerResponse>
-    {
+    pub async fn play(
+        &self,
+        hello: &ClientHello,
+        socket_id: SocketId,
+    ) -> Option<InitServerResponse> {
         let mut inner = self.inner.lock().await;
 
         println!(
@@ -140,10 +143,12 @@ impl Server {
 
         // Fails if socket_id not found
         // In prod version dont crash here?
-        let mut client = inner.get_client_mut_by_addr(socket_id).expect("client tried to /play without calling /join");
+        let mut client = inner
+            .get_client_mut_by_addr(socket_id)
+            .expect("client tried to /play without calling /join");
         client.player_client = Some(PlayerClient {
             id: client_id,
-            last_tick_us : 0,
+            last_tick_us: 0,
         });
 
         Some(InitServerResponse {
@@ -199,23 +204,44 @@ impl Server {
                         update.time_us = current_time - 1;
                     }
                 }
-
             }
 
-            let nonempty_updates : Vec<_> = client_updates.iter().filter(|(x, _)| x.input != game::Input::None).cloned().collect();
+            let mut has_updates = false;
+
+            for (x, _) in &client_updates {
+                if (x.input != game::Input::None) {
+                    has_updates = true;
+                    break;
+                }
+            }
+
+            let nonempty_updates: Vec<_> = client_updates
+                .iter()
+                .filter(|(x, _)| x.input != game::Input::None)
+                .cloned()
+                .collect();
 
             for (update, receive_time) in &nonempty_updates {
-                let receive_time_us = receive_time.saturating_duration_since(inner.start).as_micros() as u32;
+                let receive_time_us = receive_time
+                    .saturating_duration_since(inner.start)
+                    .as_micros() as u32;
                 let delta = (update.time_us as f32 - receive_time_us as f32) / 1000.;
                 //let delta = (update.time_us as i32 - inner.timeline.top_state().time_us as i32) / 1000;
-                println!("[{:?}] Update - {:?} at client time {}ms, receive_time {}ms, delta {}ms", update.player_id, update.input, update.time_us / 1000, receive_time_us / 1000, delta.floor());
+                println!(
+                    "[{:?}] Update - {:?} at client time {}ms, receive_time {}ms, delta {}ms",
+                    update.player_id,
+                    update.input,
+                    update.time_us / 1000,
+                    receive_time_us / 1000,
+                    delta.floor()
+                );
             }
 
-
-            if (nonempty_updates.len() > 0)
-            {
+            if (nonempty_updates.len() > 0) {
                 //let top_state_before = inner.timeline.top_state().clone();
-                inner.timeline.propagate_inputs(nonempty_updates.into_iter().map(|(x, _)| x).collect());
+                inner
+                    .timeline
+                    .propagate_inputs(nonempty_updates.into_iter().map(|(x, _)| x).collect());
                 /*
                 let top_state_after = inner.timeline.top_state();
 
@@ -241,9 +267,15 @@ impl Server {
             for new_player in new_players {
                 // We need to make sure this gets propagated properly
                 // Weird edge case bugs
-                println!("[{:?}] In run, adding a new player {:?}", inner.game_id, new_player);
+                println!(
+                    "[{:?}] In run, adding a new player {:?}",
+                    inner.game_id, new_player
+                );
                 let spawn_pos = find_spawn_pos(inner.timeline.top_state());
-                println!("[{:?}] Spawning new player at {:?}", inner.game_id, spawn_pos);
+                println!(
+                    "[{:?}] Spawning new player at {:?}",
+                    inner.game_id, spawn_pos
+                );
                 inner.timeline.add_player(new_player, spawn_pos);
             }
 
@@ -260,17 +292,28 @@ impl Server {
             let top_state = inner.timeline.top_state();
 
             let mut last_client_sent = PlayerIdMap::new();
-            for client in (&inner.clients).iter().filter_map(|x| x.player_client.as_ref()) {
-                inner.timeline.get_state_before_eq_us(client.last_tick_us).map(|x| {
-                    last_client_sent.set(client.id, RemoteTickState {
-                        time_us: x.time_us,
-                        states: x.get_valid_player_states(),
+            for client in (&inner.clients)
+                .iter()
+                .filter_map(|x| x.player_client.as_ref())
+            {
+                inner
+                    .timeline
+                    .get_state_before_eq_us(client.last_tick_us)
+                    .map(|x| {
+                        last_client_sent.set(
+                            client.id,
+                            RemoteTickState {
+                                time_us: x.time_us,
+                                states: x.get_valid_player_states(),
+                            },
+                        );
                     });
-                });
             }
 
             let tick = CrossyMessage::ServerTick(ServerTick {
-                exact_send_server_time_us : Instant::now().saturating_duration_since(inner.start).as_micros() as u32,
+                exact_send_server_time_us: Instant::now()
+                    .saturating_duration_since(inner.start)
+                    .as_micros() as u32,
 
                 latest: RemoteTickState {
                     time_us: top_state.time_us,
@@ -280,26 +323,36 @@ impl Server {
 
                 // If an input comes in late that affects state change then this ignores it
                 // Do we care?
-                // Do we need some lookback period here? 
-                rule_state : top_state.get_rule_state().clone(),
+                // Do we need some lookback period here?
+                rule_state: top_state.get_rule_state().clone(),
             });
+
+            if (has_updates) {
+                for (id, state) in inner.timeline.top_state().player_states.iter() {
+                    match &state.move_state {
+                        crossy_multi_core::player::MoveState::Moving(m) => {
+                            println!(
+                                "[{}] Moving->({:?}, remaining_us: {})",
+                                id.0, m.target, m.remaining_us
+                            )
+                        }
+                        _ => println!("[{}] {:?}", id.0, state.pos),
+                    }
+                }
+            }
 
             if top_state.frame_id as usize % 300 == 0 {
                 //println!("Sending tick {:?}", tick);
             }
 
-            if (self.outbound_tx.receiver_count() <= 1)
-            {
+            if (self.outbound_tx.receiver_count() <= 1) {
                 inner.empty_ticks += 1;
-            }
-            else
-            {
+            } else {
                 inner.empty_ticks = 0;
             }
 
-            const EMPTY_TICKS_THERSHOLD : u32 = 60 * 20;
-            if (inner.empty_ticks > EMPTY_TICKS_THERSHOLD)
-            {
+            const EMPTY_TICKS_THERSHOLD: u32 = 60 * 20;
+            if (inner.empty_ticks > EMPTY_TICKS_THERSHOLD) {
                 // Noone left listening, shut down
                 println!("[{:?}] Shutting down game", inner.game_id);
                 self.outbound_tx.send(CrossyMessage::GoodBye()).unwrap();
@@ -317,7 +370,13 @@ impl Server {
         }
     }
 
-    async fn receive_updates(&self) -> (Vec<(RemoteInput, Instant)>, Vec<game::PlayerId>, Vec<(game::PlayerId, bool)>) {
+    async fn receive_updates(
+        &self,
+    ) -> (
+        Vec<(RemoteInput, Instant)>,
+        Vec<game::PlayerId>,
+        Vec<(game::PlayerId, bool)>,
+    ) {
         let mut queued_messages = Vec::with_capacity(8);
 
         let mut guard = self.queued_messages.lock().await;
@@ -330,25 +389,25 @@ impl Server {
         let mut inner = self.inner.lock().await;
         let mut dropped_players = vec![];
 
-        while let Some((message, socket_id, receive_time)) = queued_messages.pop()
-        {
+        while let Some((message, socket_id, receive_time)) = queued_messages.pop() {
             match message {
                 CrossyMessage::ClientTick(t) => match inner.get_client_mut_by_addr(socket_id) {
                     Some(client) => {
-                        if let Some(player_client) = client.player_client.as_mut()
-                        {
+                        if let Some(player_client) = client.player_client.as_mut() {
                             let client_time = t.time_us;
                             player_client.last_tick_us = client_time;
 
                             ready_players.push((player_client.id, t.lobby_ready));
 
-                            client_updates.push((RemoteInput {
-                                time_us: client_time,
-                                input: t.input,
-                                player_id: player_client.id,
-                            }, receive_time));
-                        }
-                        else {
+                            client_updates.push((
+                                RemoteInput {
+                                    time_us: client_time,
+                                    input: t.input,
+                                    player_id: player_client.id,
+                                },
+                                receive_time,
+                            ));
+                        } else {
                             println!("Received client update from client who has not called /play");
                         }
                     }
@@ -362,10 +421,12 @@ impl Server {
                             dropped_players.push(player_client.id);
                         }
                     }
-                },
+                }
                 CrossyMessage::TimeRequestIntermediate(time_request) => {
                     // Just forward straight over
-                    self.outbound_tx.send(CrossyMessage::TimeRequestIntermediate(time_request)).unwrap();
+                    self.outbound_tx
+                        .send(CrossyMessage::TimeRequestIntermediate(time_request))
+                        .unwrap();
                 }
                 _ => {}
             }
@@ -380,7 +441,7 @@ impl ServerInner {
         let socket_id = self.next_socket_id;
         self.next_socket_id = SocketId(socket_id.0 + 1);
         self.clients.push(Client {
-            player_client : None,
+            player_client: None,
             socket_id,
         });
 
@@ -393,7 +454,7 @@ impl ServerInner {
                 return Some(client);
             }
         }
-    
+
         None
     }
     fn get_client_by_addr(&self, id: SocketId) -> Option<&Client> {
@@ -402,17 +463,16 @@ impl ServerInner {
                 return Some(client);
             }
         }
-    
+
         None
     }
 }
 
-fn find_spawn_pos(game_state : &crossy_multi_core::game::GameState) -> crossy_multi_core::Pos {
+fn find_spawn_pos(game_state: &crossy_multi_core::game::GameState) -> crossy_multi_core::Pos {
     for x in 7..=13 {
         for y in 7..=13 {
             let spawn_pos = game::Pos::new_coord(x, y);
-            if (!game_state.space_occupied_with_player(spawn_pos, None))
-            {
+            if (!game_state.space_occupied_with_player(spawn_pos, None)) {
                 return spawn_pos;
             }
         }
