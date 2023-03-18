7e275ac30bd6acd326214d23b2afb21edea2934d
Dan <dan.slocombe@hotmail.com>
2023-03-18 10:30:32 +0000

    Draw axis

--- web-client/js/components/graphs.js
+++ web-client/js/components/graphs.js
@@ -12,6 +12,20 @@ export function create_graph(x, y, graph_data) {
         draw : function(froggy_draw_ctx) {
 
             const ctx = froggy_draw_ctx.ctx;
+
+            ctx.strokeStyle = "black";
+            ctx.beginPath();
+            ctx.moveTo(this.x, this.y + this.height);
+            ctx.lineTo(this.x + this.width, this.y + this.height);
+            ctx.stroke();
+            ctx.closePath();
+
+            ctx.beginPath();
+            ctx.moveTo(this.x, this.y);
+            ctx.lineTo(this.x, this.y + this.height);
+            ctx.stroke();
+            ctx.closePath();
+
             ctx.strokeStyle = this.colour;
             ctx.beginPath();
             for (let i in this.graph_data.data) {
