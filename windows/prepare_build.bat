b80fc03d47405bec6dae9628c27e8d472a2ebdea
Dan <dan.slocombe@hotmail.com>
2025-01-12 21:29:17 +0000

    Prep for demo builds

---
+++ windows/prepare_build.bat
@@ -0,0 +1,13 @@
+cargo build --release
+
+rmdir builds\resources /S /Q
+rmdir builds\shaders /S /Q
+
+mkdir builds\resources
+mkdir builds\shaders
+
+robocopy resources builds\resources /s /e
+robocopy shaders builds\shaders /s /e
+robocopy dependencies builds /s /e
+
+copy D:\crossy_multi\windows\target\release\roadtoads.exe builds
